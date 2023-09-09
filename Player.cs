e6527ea7d27d081ea485a84cb0c04fbdbecae3bd
Phongsakon Konrad <124145979+pkyeager@users.noreply.github.com>
2023-09-09 13:14:43 +0200

    init Player.cs and added attributes

---
+++ Player.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace LifeQuest
+{
+    class Player
+    {
+        // Player attributes
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int gender { get; set; }
+        public string birthPlace { get; set; }
+        public string stage { get; set; }
+        public bool breastfeeding { get; set; }
+        public bool balancedDiet { get; set; }
+        public bool doDrugs { get; set; }
+        public bool graduatedHighSchool { get; set; }
+        public bool earlyGameFitness { get; set; }
+        public bool socialLife { get; set; }
+        public bool careerSuccess { get; set; }
+        public bool happyFamily { get; set; }
+        public bool goodFinance { get; set; }
+        public bool endGameFitness { get; set; }
+        public bool endGameNutrition { get; set; }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,33 +5,34 @@ namespace LifeQuest
 {
     class Game
     {
-        private static int delayMilliseconds = 50;
 
-        static void Main(string[] args)
+        private static int delayMilliseconds = 50; // init delay to 50ms
+
+        static void Main(string[] args) // entry point for program
         {
             Game game = new Game();
             game.StartGame();
         }
 
-        public void StartGame()
+        public void StartGame() // method to start the game
         {
-            SetOutputDelay(delayMilliseconds);
+            SetOutputDelay(delayMilliseconds); // set the delay for text output
             SlowlyDisplayText("Welcome to LifeQuest!");
             SlowlyDisplayText("Press any key to continue...");
             Console.ReadKey();
         }
 
-        public void SetOutputDelay(int milliseconds)
+        public void SetOutputDelay(int milliseconds) // method to set the delay for text output
         {
-            delayMilliseconds = milliseconds;
+            delayMilliseconds = milliseconds; // set the delay to the value passed in
         }
 
-        public void SlowlyDisplayText(string text)
+        public void SlowlyDisplayText(string text) // method to slowly display text
         {
             foreach (char letter in text)
             {
                 Console.Write(letter);
-                Thread.Sleep(delayMilliseconds);
+                Thread.Sleep(delayMilliseconds); // sleep for the delay in milliseconds
             }
 
             Console.WriteLine();
