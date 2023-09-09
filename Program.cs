1e303af810509a558d19e45e3f5b09dea4dcdc5e
Phongsakon Konrad <124145979+pkyeager@users.noreply.github.com>
2023-09-09 13:04:24 +0200

    init game and also added slower output to make it cooler

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace LifeQuest
+{
+    class Game
+    {
+        private static int delayMilliseconds = 50;
+
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.StartGame();
+        }
+
+        public void StartGame()
+        {
+            SetOutputDelay(delayMilliseconds);
+            SlowlyDisplayText("Welcome to LifeQuest!");
+            SlowlyDisplayText("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public void SetOutputDelay(int milliseconds)
+        {
+            delayMilliseconds = milliseconds;
+        }
+
+        public void SlowlyDisplayText(string text)
+        {
+            foreach (char letter in text)
+            {
+                Console.Write(letter);
+                Thread.Sleep(delayMilliseconds);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
